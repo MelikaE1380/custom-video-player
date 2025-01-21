5e9e8fc167b7dd07a0a1d8f4b65938ef466e058a
MelikaE1380 <melikaeskandarzadeh9@gmail.com>
2025-01-21 15:24:41 +0330

    change12

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import './App.css'
 import EmployeesDashboard from './EmployeesDashboard'
-import IosPlayer from './IosPlayer/IosPlayer'
+
 
 function App() {
 
 
   return (
     <>
-      <BrowserRouter>
-
-        <Routes>
-          <Route path='/' element={<EmployeesDashboard />} />
-
-          {/* <Route path='/ios' element={<IosPlayer />} /> */}
 
 
-        </Routes>
+       
+<EmployeesDashboard />
 
-
-      </BrowserRouter>
+         
     </>
   )
 }
